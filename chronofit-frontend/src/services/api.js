8b348d64b0de5c39bdfde0c51a3b4a29972ec0f1
0147.19 <txt0147.03@gmail.com>
2026-02-27 23:01:51 +0700

    Update api.js

--- chronofit-frontend/src/services/api.js
+++ chronofit-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000', // URL của FastAPI Backend
+  baseURL: 'https://chronochef-be.onrender.com', // URL của FastAPI Backend
   headers: {
     'Content-Type': 'application/json',
   },
